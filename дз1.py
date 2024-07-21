8c8af04153824d4bcb913de9d440a76aefc8c6a6
Tabyretca <saldomodlasjr@gmail.com>
2024-07-21 16:56:34 +0600

    Update дз1.py

--- дз1.py
+++ дз1.py
@@ -1,2 +1,7 @@
 ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+command = input("Напишите команду: ")
 result = any(word in command for word in ignore)
+if result:
+    print('Команда игнорируется')
+else:
+    print('Команда выполнена')
