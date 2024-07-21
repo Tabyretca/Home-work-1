305ffbbefe6a831af004ef1b1677b46dd3476336
Tabyretca <saldomodlasjr@gmail.com>
2024-07-21 16:47:28 +0600

    Update дз1.py

--- дз1.py
+++ дз1.py
@@ -1,3 +1,2 @@
-def ignore_command(command):
-    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
-    return any(word in command for word in ignore)
+ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+result = any(word in command for word in ignore)
